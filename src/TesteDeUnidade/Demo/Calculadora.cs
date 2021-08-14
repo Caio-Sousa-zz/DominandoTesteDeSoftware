19b4c371fd9dbdac5297c57b86b48f0e05e9f444
Caio sousa <caiyo.projeto@gmail.com>
2021-08-14 11:02:44 -0300

    Theory for demo tests.

--- src/TesteDeUnidade/Demo/Calculadora.cs
+++ src/TesteDeUnidade/Demo/Calculadora.cs
@@ -1,8 +1,9 @@
-﻿using System;
-
-namespace Demo
+﻿namespace Demo
 {
     public class Calculadora
     {
+        public double Somar(double a, double b) => a + b;
+
+        public double Dividr(double a, double b) => a / b;
     }
 }
