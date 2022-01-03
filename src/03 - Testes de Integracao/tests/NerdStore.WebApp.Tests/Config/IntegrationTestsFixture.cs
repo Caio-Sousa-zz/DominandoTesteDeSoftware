05a493f6d8f1362512627db0173e243fedebdf36
Caio Sousa <caiyo.projeto@gmail.com>
2022-01-03 09:54:24 -0300

    Include Integration tests.

--- src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
+++ src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bogus;
--- src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
+++ src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Features.Tests;
 using NerdStore.WebApp.MVC;
